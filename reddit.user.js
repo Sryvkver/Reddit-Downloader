27a836ebb5174112a793a98abdd7107376d32ce1
felixire <20717854+felixire@users.noreply.github.com>
2020-10-19 16:38:45 +0200

    Update reddit.user.js
    Fixed settings button not working.
    Added a settings button next to the Profile button.

--- reddit.user.js
+++ reddit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Downloader
 // @namespace    https://github.com/felixire/Reddit-Downloader
-// @version      0.1.4
+// @version      0.1.5
 // @description  Download your saved posts or directly posts from your feed with support for Direct links (png, jpg, gif, mp4...), (Gypcat kinda), Redgify, Imgur (Only when supplied with an API key)
 // @author       felixire
 // @match        https://www.reddit.com/*
@@ -65,13 +65,40 @@ function waitForElements(selectors, timeout = 1000) {
 function createNotification(title, text){
     GM_notification({
         title,
-        text
+        text,
+        timeout: 5000
     })
+}
+
+async function addSettingsButton(){
+    waitForElements('#change-username-tooltip-id', 5000)
+    .then(parent => {
+        let chatButton = parent[0].children[0];
+        let settingsButton = chatButton.cloneNode(true);
+
+        settingsButton.setAttribute('title', 'Reddit Downloader Settings');
+        settingsButton.querySelector('a').href = '#';
+        settingsButton.querySelector('a').onclick = () => {
+            GM_config.open();
+        };
+        settingsButton.querySelector('svg').setAttribute('viewBox', '0 0 24 24');
+        //Icon link: https://iconmonstr.com/gear-1-svg/
+        settingsButton.querySelector('path').setAttribute('d', 'M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z');
+        parent[0].appendChild(settingsButton);
+    })
+    .catch(err => {
+        addSettingsButton();
+    });
 }
 //#endregion
 
 //#region Location listener and Post Download button addener
 (async () => {
+    if (window.top != window.self) 
+    {
+        return;
+    }
+
     let isAdded = false;
     let username = await getUsername();
 
@@ -3902,6 +3929,11 @@ class RedditDownloader {
 })()
 
 window.addEventListener('load', async () => {
+    if (window.top != window.self) 
+    {
+        return;
+    }
+
     await wait(100);
     window.RedditDownloader = new RedditDownloader();
 
@@ -3943,7 +3975,9 @@ window.addEventListener('load', async () => {
                 'default': 'Reddit/Stuff/Stuff/'
             }
         }
-    })
+    });
+
+    addSettingsButton();
 
     GM_registerMenuCommand('Manage Settings', (() => {
         GM_config.open();
